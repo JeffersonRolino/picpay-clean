f0ceffaa5b0de514bc5a5ebc03e945b2524d2740
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-26 20:27:19 -0300

    feat: create TransactionPin entity

---
+++ core/src/main/java/jeffersonrolino/com/github/core/domain/TransactionPin.java
@@ -0,0 +1,111 @@
+package jeffersonrolino.com.github.core.domain;
+
+import jeffersonrolino.com.github.core.exception.TransactionPinException;
+import jeffersonrolino.com.github.core.exception.enums.ErrorCodeEnum;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class TransactionPin {
+    private Long id;
+    private User user;
+    private String pin;
+    private Integer attempt;
+    private Boolean blocked;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public TransactionPin() {
+    }
+
+    public TransactionPin(User user, String pin, Integer attempt, Boolean blocked) {
+        this.user = user;
+        this.pin = pin;
+        this.attempt = attempt;
+        this.blocked = blocked;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public TransactionPin(Long id, User user, String pin, Integer attempt, Boolean blocked, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.user = user;
+        this.pin = pin;
+        this.attempt = attempt;
+        this.blocked = blocked;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) throws TransactionPinException {
+        pinIsValid(pin);
+        this.pin = pin;
+    }
+
+    public Integer getAttempt() {
+        return attempt;
+    }
+
+    public void setAttempt(Integer attempt) {
+        this.attempt = attempt;
+    }
+
+    public Boolean getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(Boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    private void pinIsValid(String pin) throws TransactionPinException {
+        if(pin.length() != 8){
+            throw new TransactionPinException(ErrorCodeEnum.TRP0001.getMessage(), ErrorCodeEnum.TRP0001.getCode());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TransactionPin that = (TransactionPin) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
