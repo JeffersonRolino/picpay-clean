e36535da5e6c6780f6f93f4b485d32873fe99731
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-26 20:30:47 -0300

    feat: create BadRequestException exception

---
+++ core/src/main/java/jeffersonrolino/com/github/core/exception/BadRequestException.java
@@ -0,0 +1,11 @@
+package jeffersonrolino.com.github.core.exception;
+
+public class BadRequestException extends Exception {
+
+    private String code;
+
+    public BadRequestException(String message, String code ) {
+        super(message);
+        this.code = code;
+    }
+}
