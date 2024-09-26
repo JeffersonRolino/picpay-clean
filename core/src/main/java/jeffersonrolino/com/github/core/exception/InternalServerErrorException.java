7a0e5db157f7b8b825124dd5f81fe83df5c540e8
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-26 20:31:09 -0300

    feat: create InternalServerErrorException exception

---
+++ core/src/main/java/jeffersonrolino/com/github/core/exception/InternalServerErrorException.java
@@ -0,0 +1,11 @@
+package jeffersonrolino.com.github.core.exception;
+
+public class InternalServerErrorException extends Exception {
+
+    private String code;
+
+    public InternalServerErrorException(String message, String code ) {
+        super(message);
+        this.code = code;
+    }
+}
